0979620a4cbdc6f95ebdf128c7b3783dc6763cf3
cristhian_menjivar <cristhian_menjivar@credicomer.com.sv>
2021-08-27 10:43:19 -0600

    fixed error

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import WaveUI from "wave-ui";
-import "wave-ui/dist/wave-ui.css";
-
 const app = createApp(App);
-
-new WaveUI(app, {
-  // Some Wave UI options.
-});
 
 app.mount("#app");
